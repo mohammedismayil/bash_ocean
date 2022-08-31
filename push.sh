63e18e021f9a0155c39aa74801e9ceaf0e205f70
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-08-31 22:52:25 +0530

    pushed using push script

--- push.sh
+++ push.sh
@@ -4,10 +4,10 @@
  if [ "$#" -eq  "0" ]
    then
      git commit -m "Changes"
-     git push
+     git push origin master
      echo "No commit message has been passed"
  else
      git commit -m "$1"
-     git push
+     git push origin master
      echo "Commit message has been set to $1"
  fi
