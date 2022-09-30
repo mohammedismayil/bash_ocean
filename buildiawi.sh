e9418d6b7d91932aab41df4874115f5946949092
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-09-30 09:21:21 +0530

    Changes

--- buildiawi.sh
+++ buildiawi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "$(echo "hello $1")"
-job="$(curl --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$2" --form "token=$1")"
+job1="$(curl --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$2" --form "token=$1")"
 #result="$(curl --location -s --request GET 'https://upload.diawi.com/status?token=LOVzCmMH1mjTPi4Wv2dmwVAZ02LWqDLALo6HlVPtxm&job=CiDWEKJaZ7kmvQAWHR4OcCxJYEcGltYMvH1HyiGn80')"
-echo "result: '$job'"
+
+job2="$(curl --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$2" --form "token=$1")"
+
+echo "result: '$job1'"
+echo "result: '$job2'"
+
