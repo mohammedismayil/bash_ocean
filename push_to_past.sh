f9cddad6f55130389d0174b5c7c9a892ea3d6954
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-10-03 08:31:38 +0530

    # Please include a useful commit message!

---
+++ push_to_past.sh
@@ -0,0 +1,13 @@
+
+ #!/bin/bash
+ git add -A
+ if [ "$#" -eq  "0" ]
+   then
+     git commit --date="10 day ago" -m "Changes"
+     git push origin main
+     echo "No commit message has been passed"
+ else
+     git commit -m "$1" --date="$2"
+     git push origin main
+     echo "Commit message has been set to $1"
+ fi
