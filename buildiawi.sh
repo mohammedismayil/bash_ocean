4343eed9d564d05d1c3511410c98c3543c70cb1e
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-10-31 21:44:16 +0530

    # Please include a useful commit message!

--- buildiawi.sh
+++ buildiawi.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 flutter build ipa --export-options-plist=exportOptions.plist
-job1="$(curl --http1.1 --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$(pwd)/build/ios/ipa/PayBit.ipa" --form "token=$1" | jq -r '.job')"
-sleep 6
+job1="$(curl --http1.1 --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$(pwd)/build/ios/ipa/CliqBuy.ipa" --form "token=$1" | jq -r '.job')"
+sleep 10
 job2="$(curl --location -s --request GET "https://upload.diawi.com/status?token=$1&job=$job1" | jq -r '.link')"
 
 flutter build apk
 job3="$(curl --http1.1 --location --request POST "https://upload.diawi.com/?token=$1&file" --form "file=@$(pwd)/build/app/outputs/flutter-apk/app-release.apk" --form "token=$1" | jq -r '.job')"
-sleep 6
+sleep 10
 job4="$(curl --location -s --request GET "https://upload.diawi.com/status?token=$1&job=$job3" | jq -r '.link')"
 echo "iOS: '$job2'"
 echo "Android: '$job4'"
+echo "$(date)"
+echo "$(pwd)"
+
+echo "" >> buildlinks.txt
+
+echo "iOS: '$job2' -- Generated on $(date)" >> buildlinks.txt
+echo "Android: '$job4' -- Generated on $(date)" >> buildlinks.txt
