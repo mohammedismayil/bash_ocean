059e121cf7f1e92b0771193222c4918bd1cebb43
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-08-15 20:21:45 +0530

    # Please include a useful commit message!

---
+++ OverthewireWargame/Bandit/level_8.sh
@@ -0,0 +1,5 @@
+#The password for the next level is stored in the file data.txt next to the word millionth
+cd inhere
+find -name data.txt
+grep millionth data.txt
+#TESKZC0XvTetK0S9xNwm25STk5iWrBvP
