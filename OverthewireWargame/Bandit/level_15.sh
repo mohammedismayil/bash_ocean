97d7c50ec8d8dc28d2bacddf7886fe904f81a460
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-08-22 23:34:41 +0530

    level 15

---
+++ OverthewireWargame/Bandit/level_15.sh
@@ -0,0 +1,4 @@
+#The password for the next level can be retrieved by submitting the password of the current level to port 30000 on localhost.
+cat /etc/bandit_pass/bandit14
+nc localhost 30000
+#jN2kgmIXJ6fShzhT2avhotn4Zcka6tnt
