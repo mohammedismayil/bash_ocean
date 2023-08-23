3b9fc15732ebc918d5ab0045921ed669ffdf0254
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-08-23 23:00:55 +0530

    level 16

--- OverthewireWargame/Bandit/level_16.sh
+++ OverthewireWargame/Bandit/level_16.sh
@@ -1,2 +1,5 @@
 #The password for the next level can be retrieved by submitting the password of the current level to port 30001 on localhost using SSL encryption.
 #Helpful note: Getting “HEARTBEATING” and “Read R BLOCK”? Use -ign_eof and read the “CONNECTED COMMANDS” section in the manpage. Next to ‘R’ and ‘Q’, the ‘B’ command also works in this version of that command…
+openssl  s_client -connect localhost:30001
+#enter the current (previous level password) - it will show the next level password
+#JQttfApK4SeyHwDlI9SXGR50qclOAil1
