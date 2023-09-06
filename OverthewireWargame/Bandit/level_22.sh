c3afff8acfc3686c7e5048cdb5bc5097ea1cf522
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-09-06 23:55:43 +0530

    Some Changes

--- OverthewireWargame/Bandit/level_22.sh
+++ OverthewireWargame/Bandit/level_22.sh
@@ -1,2 +1,9 @@
 # A program is running automatically at regular intervals from cron, the time-based job scheduler. Look in /etc/cron.d/ for the configuration and see what command is being executed.
 ls -la /etc/cron.d
+cd /etc/cron.d
+ls
+cat cronjob_bandit22
+cat /usr/bin/cronjob_bandit22.sh
+#get temp path from this
+cat /tmp/t7O6lds9S0RqQh9aMcz6ShpAoZKF7fgv
+#WdDozAdTM2z9DiFEQ2mGlwngMfj4EZff
