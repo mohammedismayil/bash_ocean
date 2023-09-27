a76892b55597172e0553c8faaad6db60a0fb0968
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-09-27 17:48:37 +0530

    # Please include a useful commit message!

--- OverthewireWargame/Bandit/level_23.sh
+++ OverthewireWargame/Bandit/level_23.sh
@@ -1,3 +1,8 @@
 # A program is running automatically at regular intervals from cron, the time-based job scheduler. Look in /etc/cron.d/ for the configuration and see what command is being executed.
 
 # NOTE: Looking at shell scripts written by other people is a very useful skill. The script for this level is intentionally made easy to read. If you are having problems understanding what it does, try executing it to see the debug information it prints.
+ls -la /etc/cron.d
+cat /etc/cron.d/cronjob_bandit23
+echo I am user bandit23 | md5sum | cut -d ' ' -f 1
+cat /tmp/8ca319486bfbbc3663ea0fbe81326349
+#QYw0Y2aiA672PsMmh9puTQuhoz8SyR2G
