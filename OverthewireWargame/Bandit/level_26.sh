3bdf5ee055272db238219dbad2db5c65acf25d56
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-10-07 07:27:59 +0530

    level 26 initiated

---
+++ OverthewireWargame/Bandit/level_26.sh
@@ -0,0 +1,3 @@
+# Bandit Level 25 → Level 26
+# Level Goal
+# Logging in to bandit26 from bandit25 should be fairly easy… The shell for user bandit26 is not /bin/bash, but something else. Find out what it is, how it works and how to break out of it.
