1d67f1b143ec1a643155560a4eea5a9351d5e66a
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-10-10 07:56:17 +0530

    level 26 initiated

--- OverthewireWargame/Bandit/level_26.sh
+++ OverthewireWargame/Bandit/level_26.sh
@@ -1,3 +1,8 @@
 # Bandit Level 25 → Level 26
 # Level Goal
 # Logging in to bandit26 from bandit25 should be fairly easy… The shell for user bandit26 is not /bin/bash, but something else. Find out what it is, how it works and how to break out of it.
+
+cat /etc/passwd | grep bandit26
+ls -la /usr/bin/showtext
+cat /usr/bin/showtext
+ssh -i bandit26.sshkey bandit26@localhost -p 2220
